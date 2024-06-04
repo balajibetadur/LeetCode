59d8f5b3101ec87e881b037538eabfe2c2b596fe
balaji betadur <balajibetadur@gmail.com>
2024-06-05 02:20:57 +0530

    Time: 35 ms (70.39%) | Memory: 16.6 MB (80.62%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,12 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        hash = {}
-        for ch in s: hash[ch] = hash.get(ch, 0) + 1
+        # approach 1
+        # hash = {}
+        # for ch in s: hash[ch] = hash.get(ch, 0) + 1
+        # res = 0
+        # for ch, count in hash.items(): 
+        #     res += (count // 2) * 2
+        #     if res % 2 == 0 and count % 2 == 1: res += 1
+        # return res
 
-        res = 0
+        #approach 2
+        temp = []
+        for ch in s:
+            if ch in temp: temp.remove(ch)
+            else: temp.append(ch)
 
-        for ch, count in hash.items(): 
-            res += (count // 2) * 2
-            if res % 2 == 0 and count % 2 == 1: res += 1
-
-        return res
+        if len(temp) == 0: return len(s)
+        else: return len(s) - len(temp) + 1
