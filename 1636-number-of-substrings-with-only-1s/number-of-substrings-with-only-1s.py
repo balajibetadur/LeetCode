ec919c54273746b34170acfc8612284710d17da2
balaji betadur <balajibetadur@gmail.com>
2024-07-03 22:21:39 +0530

    Time: 51 ms (69.74%) | Memory: 17.6 MB (22.51%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -1,11 +1,3 @@
 class Solution:
     def numSub(self, s: str) -> int:
-        count = 0
-        count_one = 0
-        for ch in s:
-            if ch == '0': 
-                count += ((count_one * (count_one + 1)) / 2) % (10 ** 9 + 7)
-                count_one = 0
-            else: count_one += 1
-        count += ((count_one * (count_one + 1)) / 2) % (10 ** 9 + 7)
-        return int(count)
+        return sum([(len(a)*(len(a)+1))//2 for a in s.split("0")]) % (pow(10,9) + 7)
